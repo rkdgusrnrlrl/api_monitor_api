3a79a220ea99a59b28c8c62da3e863c7cae20a96
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-03-11 20:17:09 +0900

    feature:add api requeset model

--- state_monitor/models.py
+++ state_monitor/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import json
 
 class APICallLog(models.Model):
     call_id = models.AutoField(primary_key=True)
@@ -9,3 +10,34 @@ class APICallLog(models.Model):
     req_body = models.TextField()
     res_body = models.TextField()
     res_second = models.FloatField()
+
+
+class APIMonitorRequest(models.Model):
+    REQ_METHOD_CHOICES = (
+        ('GET', 'GET'),
+        ('POST', 'POST'),
+        ('PUT', 'PUT'),
+        ('DELETE', 'DELETE'),
+    )
+
+    api_req_id = models.AutoField(primary_key=True)
+    api_name = models.CharField(max_length=100)
+    api_base_url = models.TextField()
+    req_body = models.TextField()
+    req_method = models.CharField(
+        max_length=6,
+        choices=REQ_METHOD_CHOICES,
+        default="GET"
+    )
+    req_qs = models.TextField()
+
+    def get_body(self):
+        try:
+            return json.loads(self.req_body)
+        except ValueError:
+            return {}
+
+    def set_req_body(self, req_body):
+        json.load(req_body)
+        self.req_body = req_body
+
