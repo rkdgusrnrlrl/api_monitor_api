d20594a833b364b53886e5b0c2a9a606d67c03c3
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-04-02 23:18:50 +0900

    feat:run_task api 추가

--- state_monitor/views.py
+++ state_monitor/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
 from .models import APICallLog, APIMonitorRequest
 from rest_framework import viewsets
-from rest_framework.views import APIView
 from .serializers import APICallLogSerializer, APIMonitorRequestSerializer
+from rest_framework.decorators import detail_route, list_route
+from rest_framework.response import Response
 
 
 class APICallLogViewSet(viewsets.ModelViewSet):
@@ -13,3 +13,9 @@ class APICallLogViewSet(viewsets.ModelViewSet):
 class APIMonitorRequestViewSet(viewsets.ModelViewSet):
     queryset = APIMonitorRequest.objects.all().order_by('-created_at')
     serializer_class = APIMonitorRequestSerializer
+
+    @detail_route(methods=['post'])
+    def run_task(self, request, pk=None):
+        api_request = APIMonitorRequest.objects.get(pk=pk)
+        serializer = self.get_serializer(api_request)
+        return Response(serializer.data)
