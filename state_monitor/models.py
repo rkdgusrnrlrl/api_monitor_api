b35dc66b114edf34c0567f4855c4eaf8cc3c917b
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-08-31 15:47:39 +0900

    Test function register api, api_status - test set wrong value to choice field

--- state_monitor/models.py
+++ state_monitor/models.py
@@ -34,3 +34,27 @@ class APIStatus(models.Model):
     check_time = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=10, choices=API_STATUS_CHOICES)
     api = models.ForeignKey(API, on_delete=models.CASCADE)
+
+
+def validate_method_choice(sender, instance, **kwargs):
+    valid_methods = [t[0] for t in METHOD_CHOICES]
+    if instance.method not in valid_methods:
+        from django.core.exceptions import ValidationError
+        raise ValidationError(
+            'API Method "{}" is not one of the permitted values: {}'.format(
+                instance.method,
+               ', '.join(valid_methods)))
+
+
+def validate_status_choice(sender, instance, **kwargs):
+    valid_status = [t[0] for t in sender.API_STATUS_CHOICES]
+    if instance.status not in valid_status:
+        from django.core.exceptions import ValidationError
+        raise ValidationError(
+            'API Method "{}" is not one of the permitted values: {}'.format(
+                instance.status,
+               ', '.join(valid_status)))
+
+
+models.signals.pre_save.connect(validate_method_choice, sender=API)
+models.signals.pre_save.connect(validate_status_choice, sender=APIStatus)
--- state_monitor/tests/test_models.py
+++ state_monitor/tests/test_models.py
@@ -1,6 +1,6 @@
 import pytest
-from state_monitor.models import Application, API
-
+from state_monitor.models import Application, API,APIStatus
+from django.core.exceptions import ValidationError
 
 @pytest.mark.django_db
 def test_create_application():
@@ -13,6 +13,7 @@ def test_create_application():
 def test_create_api():
     app_name = 'hello123'
     app = Application.objects.create(name=app_name)
+
     api_name = '사용자 등록 API'
     api = API.objects.create(name=api_name, full_url='http://api.dakbutfly.me/user', method='POST', application=app)
     assert api_name == api.name
@@ -20,8 +21,33 @@ def test_create_api():
 
 @pytest.mark.django_db
 def test_when_wrong_method_should_be_fail():
+    with pytest.raises(ValidationError):
+        app_name = 'hello123'
+        app = Application.objects.create(name=app_name)
+
+        api_name = '사용자 등록 API'
+        api = API.objects.create(name=api_name, full_url='http://api.dakbutfly.me/user', method='SHUT', application=app)
+
+
+@pytest.mark.django_db
+def test_create_api_status():
     app_name = 'hello123'
     app = Application.objects.create(name=app_name)
+
     api_name = '사용자 등록 API'
-    api = API.objects.create(name=api_name, full_url='http://api.dakbutfly.me/user', method='SHUT', application=app)
-    assert api_name == api.name
+    api = API.objects.create(name=api_name, full_url='http://api.dakbutfly.me/user', method='POST', application=app)
+
+    api_status = APIStatus.objects.create(api=api, status='SUCCESS')
+    assert api_status.status == 'SUCCESS'
+
+
+@pytest.mark.django_db
+def test_when_wrong_status_should_be_raise_validation_error():
+    with pytest.raises(ValidationError):
+        app_name = 'hello123'
+        app = Application.objects.create(name=app_name)
+
+        api_name = '사용자 등록 API'
+        api = API.objects.create(name=api_name, full_url='http://api.dakbutfly.me/user', method='POST', application=app)
+
+        api_status = APIStatus.objects.create(api=api, status='WRONG')
